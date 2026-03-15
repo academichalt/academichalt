930a9c04f74cd15d004e3961466e0580a1e8877f
academichalt <academichalt@gmail.com>
2026-03-15 16:56:21 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://www.jeehalt.in',
+  site: 'https://www.academichalt.com',
   integrations: [tailwind()],
   output: 'static',
 });
